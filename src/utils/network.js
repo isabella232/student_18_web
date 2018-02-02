90496ab5f1d05bf398e07cf74c5a49ef34b63ef3
Jeff R. Allen <jeff.allen@epfl.ch>
2018-02-02 16:50:52 +0100

    A makefile to remind me how to deploy this next time.

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,4 +1,31 @@
-import splitHost from 'split-host'
+// From:
+// https://www.npmjs.com/package/split-host
+// but it had syntax errors in it I had to fix, so I pasted it in here.
+var isPort = function isPort(value) {
+  return typeof value === 'number' && value >= 0 && value <= 65534 && Math.floor(value) === value;
+};
+
+function splitHost(host) {
+  if (host === undefined) {
+    return;
+  }
+
+  host = String(host);
+  var i = host.lastIndexOf(':');
+
+  if (i === -1) {
+    var _port = +host;
+    return isPort(_port) ? { port: _port } : { host: host, hostname: host };
+  }
+
+  var port = +host.slice(i + 1);
+  if (isPort(port)) {
+    var hostname = host.slice(0, i);
+    return { host: hostname, hostname, port };
+  }
+
+    return { host: host, hostname: host };
+};
 
 /**
  * Transform a tcp address to the corresponding WS address because the Cothority uses the port + 1 of the tcp address
